663d93d067f251d2256ef943b1a2154f1c82a666
mgiacomi <mgiacomi@gltech.com>
2016-08-24 08:18:29 -0700

    mobile fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     hh, mm = mins.divmod(60)
     "%dh %dm" % [hh, mm]
   end
+
+  def days_to_end
+    (Rails.configuration.end_date - Date.today).to_i
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.end_date = Date.strptime("9/1/2016", "%m/%d/%Y")
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
