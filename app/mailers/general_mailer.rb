532aa9149eaacfba507d5bc9bde6ab0a153c6d3a
mgiacomi <mgiacomi@gltech.com>
2016-10-04 21:21:48 -0700

    fix welcome email

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,7 +2,7 @@ class GeneralMailer < ApplicationMailer
 
   def welcome_flyer student
     @student = student
-    mail(to: student.email, subject: "#{student.first} welcome to the Richmond Read-A-Thon")
+    mail(to: student.email, subject: "#{student.first}, welcome to the Richmond Read-A-Thon")
   end
 
 end
--- app/mailers/pledge_mailer.rb
+++ app/mailers/pledge_mailer.rb
@@ -7,8 +7,9 @@ class PledgeMailer < ApplicationMailer
     mail(to: email, subject: "Help support #{student.first}'s Read-A-Thon")
   end
 
-  def email2 emails, name, profile_pledge_bitly_url, student
+  def email2 emails, name, message, profile_pledge_bitly_url, student
     @name = name
+    @message = message
     @profile_pledge_bitly_url = profile_pledge_bitly_url
     @student = student
     mail(to: emails, subject: "Help support #{student.first}'s Read-A-Thon")
