b118d5541cbc813fe7874d503e41b8e86e405634
mgiacomi <mgiacomi@gltech.com>
2016-10-14 02:49:21 -0700

    Fixes to student query screens.

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,7 +2,8 @@ class GeneralMailer < ApplicationMailer
 
   def welcome_flyer student
     @student = student
-    mail(to: student.email, subject: "#{student.first}, welcome to the Richmond Read-A-Thon")
+    to = student.email2.nil? ? student.email : "#{student.email}, #{student.email2}"
+    mail(to: to, subject: "#{student.first}, welcome to the Richmond Read-A-Thon")
   end
 
 end
