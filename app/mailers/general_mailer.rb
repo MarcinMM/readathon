4772299c64062b5c4b6796385a44574f50c1d530
mgiacomi <mgiacomi@gltech.com>
2016-11-08 01:26:25 -0800

    Last Welcome Flyer

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -3,7 +3,7 @@ class GeneralMailer < ApplicationMailer
   def welcome_flyer student
     @student = student
     to = student.email2.nil? ? student.email : "#{student.email}, #{student.email2}"
-    mail(to: to, subject: "#{student.first}, welcome to the Richmond Read-A-Thon")
+    mail(to: to, subject: "The Richmond Read-A-Thon is almost over!")
   end
 
 end
