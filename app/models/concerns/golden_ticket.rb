d0c42dded725e78c54546f99d04825d0ac3c3f32
mgiacomi <Rebeka13!>
2018-02-04 16:40:31 -0800

    More golden ticket data.

--- app/models/concerns/golden_ticket.rb
+++ app/models/concerns/golden_ticket.rb
@@ -4,12 +4,16 @@ module GoldenTicket
   module ClassMethods
   end
 
+  def percent_boost
+    20
+  end
+
   def total_minutes
     Student.joins(:readlogs).where("students.teacher_id=? and readlogs.teacher_id is null", self.id).sum("readlogs.minutes")
   end
 
   def gt_percent
-    ((total_minutes.to_f / gt_total_minutes.to_f) * 100).round(0)
+    ((total_minutes.to_f / gt_total_minutes.to_f) * 100).round(0) + percent_boost
   end
 
   def gt_total_minutes
