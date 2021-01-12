16a53b7c3755fcf248bfe41b708886945084044a
MarcinM <marcinmm@fastmail.fm>
2021-01-11 19:37:28 -0800

    Removing unused teacher/user code.

--- app/controllers/rmgr/teachers_controller.rb
+++ app/controllers/rmgr/teachers_controller.rb
@@ -33,13 +33,13 @@ class Rmgr::TeachersController < ApplicationController
   def update
     @teacher = Teacher.find(params[:id])
 
-    if @teacher.user.email != params[:teacher][:user][:email]
-      @teacher.user.update_attribute :email, params[:teacher][:user][:email]
-    end
+    # if @teacher.user.email != params[:teacher][:user][:email]
+    #   @teacher.user.update_attribute :email, params[:teacher][:user][:email]
+    # end
 
-    if params[:teacher][:user][:password].length > 0
-      @teacher.user.update_attribute :password, params[:teacher][:user][:password]
-    end
+    # if params[:teacher][:user][:password].length > 0
+    #   @teacher.user.update_attribute :password, params[:teacher][:user][:password]
+    # end
 
     if @teacher.update_attributes(teacher_params(params))
       redirect_to rmgr_teachers_path, notice: {title: 'Success', msg: 'Teacher was successfully updated.'}
@@ -50,9 +50,9 @@ class Rmgr::TeachersController < ApplicationController
 
   private
 
-  def user_params params
-    params.require(:user).permit(:email, :password)
-  end
+  # def user_params params
+  #   params.require(:user).permit(:email, :password)
+  # end
 
   def teacher_params params
     params.require(:teacher).permit(:name, :grade)
